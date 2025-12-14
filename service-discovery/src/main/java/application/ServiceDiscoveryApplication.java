1c8e95ac6b7d3719ed5c9d6e3f382d57100a1534
Юрий <urabaket98@gmail.com>
2025-12-14 14:50:21 +0500

    Добавление service-discovery: -создан service-discovery; -добавлены клиенты notification-service и user-service

--- service-discovery/src/main/java/application/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/application/ServiceDiscoveryApplication.java
@@ -2,13 +2,14 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class NotificationServiceApplication {
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
 
 }
